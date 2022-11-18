8b8fac52d7c5c570e9a6917248d6618cf3c00657
Arif Uddin <mr.uddin360@gmail.com>
2022-11-18 20:07:35 +0600

    avilable appointment added

--- src/Components/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Components/Appointment/AvailableAppointments/AppointmentOption.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AppointmentOption = ({ appointmentOption, setTreatment }) => {
     const { name, slots } = appointmentOption;
-    
+
     return (
         <div className="card shadow-xl">
             <div className="card-body text-center">
